ca7b55a42fa2b09ca1617acf611b1799687981c1
gurxt <jung_jungian@outlook.com>
2023-06-09 11:08:56 -0400

    set up redux

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import {  Text, View } from 'react-native'
+import { Provider } from 'react-redux'
+import { store } from './store'
 
-export default function App() {
+// configure redux.
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <Provider store={store}>
+        <View>
+            <Text>LOL</Text>
+        </View>
+    </Provider>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
---
+++ README.md
@@ -0,0 +1,8 @@
+### Tech Stack
+
+1. React-Native with Expo
+2. React Navigation
+3. Redux
+4. Nativewind (TailwindCSS)
+5. Google Auth
+6. Firebase
