d20bea19b5130f57eecf13586db297d8e83b11fa
gurxt <jung_jungian@outlook.com>
2023-06-11 07:52:09 -0400

    working on user profile

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,5 +1,5 @@
-import { View, Text } from 'react-native'
-import React, { useEffect } from 'react'
+import { View, Text, StyleSheet, StatusBar } from 'react-native'
+import React from 'react'
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
 import { GOOGLE_MAPS_APIKEY } from '@env'
 import { SafeAreaView } from 'react-native-safe-area-context'
@@ -9,50 +9,67 @@ import { setDestination, setOrigin } from '../slices/navSlice'
 import { useNavigation } from '@react-navigation/native'
 
 const valley = require('./../assets/valley.png')
+const user_img = require("./../assets/female_01.png")
 
 const SearchScreen = () => {
     const dispatch = useDispatch()
     const navigation = useNavigation()
 
     return (
-        <SafeAreaView className="flex-1 bg-white">
-            <Image source={valley} className="w-full h-40" />
-            <View className="flex-1 justify-center pt-2 pb-2 bg-black">
-                <GooglePlacesAutocomplete
-                    placeholder="Enter Location"
-                    styles={{
-                        container: {
-                            justifySelf: 'center'
-                        },
-                        textInput: {
-                            fontSize: 14
-                        }
-                    }}
-                    onPress={(data, details = null) => {
-                        dispatch(setOrigin({
-                            location: details.geometry.location,
-                            description: data.description 
-                        }))
+        <SafeAreaView className="flex-1 bg-black">
+            <StatusBar />
+            <View className="flex-1 justify-center items-center">
+                <View className="justify-center items-center">
+                    <Text className="text-white m-4 text-lg">Welcome, Tim Zoleta!</Text>
+                    <Image source={user_img} className="h-32 w-32 rounded-full" />
+                </View>
+                <View className="flex-1 w-full m-4">
+                    <GooglePlacesAutocomplete
+                        placeholder="Start Destination"
+                        styles={googleStyles}
+                        onPress={(data, details = null) => {
+                            dispatch(setOrigin({
+                                location: details.geometry.location,
+                                description: data.description 
+                            }))
 
-                        dispatch(setDestination(null))
+                            dispatch(setDestination(null))
 
-                        navigation.navigate("MapScreen")
-                    }}
-                    query={{
-                        key: GOOGLE_MAPS_APIKEY,
-                        language: "en",
-                        components: "country:ca"
-                    }}
-                    nearbyPlacesAPI="GooglePlacesSearch"
-                    debounce={400}
-                    enablePoweredByContainer={false}
-                    minLength={3}
-                    fetchDetails={true}
-                    returnKeyType={"search"}
-                />
-            </View>
+                            navigation.navigate("MapScreen")
+                        }}
+                        query={{
+                            key: GOOGLE_MAPS_APIKEY,
+                            language: "en",
+                            components: "country:ca"
+                        }}
+                        nearbyPlacesAPI="GooglePlacesSearch"
+                        debounce={400}
+                        enablePoweredByContainer={false}
+                        minLength={3}
+                        fetchDetails={true}
+                        returnKeyType={"search"}
+                    />
+                </View>
+            </View> 
         </SafeAreaView>
     )
 }
+
+
+const googleStyles = StyleSheet.create({
+    container: {
+        paddingTop: 10,
+        flex: 0
+    },
+    textInput: {
+        backgroundColor: "#DDD",
+        borderRadius: 10,
+        fontSize: 14
+    },
+    textInputContainer: {
+        paddingHorizontal: 20,
+        paddingBottom: 20
+    }
+})
 
 export default SearchScreen 
