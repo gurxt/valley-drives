9f5480c928fe041ddad4bade6ad3de7527689e28
gurxt <jung_jungian@outlook.com>
2023-06-15 16:45:26 -0400

    added functionality to clear search result

--- components/MapSearch.js
+++ components/MapSearch.js
@@ -1,23 +1,35 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useRef } from 'react'
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
 import { GOOGLE_MAPS_APIKEY } from '@env'
 import { useDispatch } from 'react-redux'
 import { setDestination, setOrigin } from '../slices/navSlice'
 import { useNavigation } from '@react-navigation/native'
 import { StyleSheet } from 'react-native'
+import { XMarkIcon } from 'react-native-heroicons/solid'
 
 const MapSearch = ({ option }) => {
     const dispatch = useDispatch()
     const navigation = useNavigation()
+    const searchTextRef = useRef(null)
 
     return (
         <View backgroundColor="#9fc9bebb" className="flex-1 w-full pt-4">
             <GooglePlacesAutocomplete
                 placeholder={ option ? 'Start Destination' : 'End Destination' }
+                renderRightButton={() => (
+                    <View style={googleStyles.button}>
+                        <XMarkIcon 
+                            size={20} 
+                            color="azure" 
+                            onPress={() => searchTextRef.current.clear()}
+                        />
+                    </View>
+                )}
                 styles={googleStyles}
                 textInputProps={{
-                    placeholderTextColor: "white"
+                    ref: searchTextRef,
+                    placeholderTextColor: "azure"
                 }}
                 onPress={(data, details = null) => {
                     if (option === "destination") {
@@ -60,7 +72,12 @@ const googleStyles = StyleSheet.create({
     textInput: {
         backgroundColor: "#80847e",
         fontSize: 14,
-        color: "white"
+        color: "white",
+        height: "100%",
+        borderTopLeftRadius: 5,
+        borderBottomLeftRadius: 5,
+        borderBottomRightRadius: 0,
+        borderTopRightRadius: 0
     },
     textInputContainer: {
         paddingHorizontal: 10,
@@ -80,6 +97,14 @@ const googleStyles = StyleSheet.create({
         textShadowColor: "rgba(0,0,0,0.5)",
         textShadowOffset: { width: 1, height: 2},
         textShadowRadius: 6
+    },
+    button: {
+        borderBottomRightRadius: 5,
+        borderTopRightRadius: 5,
+        justifyContent: "center",
+        alignItems: "center",
+        width: "10%",
+        backgroundColor: "#80847eaa",
     }
 })
 
