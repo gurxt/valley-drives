b896f93479dcc1773cf33c7d19717cc2408210e3
gurxt <jung_jungian@outlook.com>
2023-06-16 20:17:39 -0400

    added taxi schema

--- valley-drives-sanity/schemas/index.js
+++ valley-drives-sanity/schemas/index.js
@@ -1,3 +1,4 @@
 import user from './user'
+import taxi from './taxi'
 
-export const schemaTypes = [ user ]
+export const schemaTypes = [ user, taxi ]
---
+++ valley-drives-sanity/schemas/taxi.js
@@ -0,0 +1,55 @@
+import { defineType } from 'sanity'
+
+/*
+{
+            name: "Timmy's Taxi",
+            logo: Taxi1,
+            colors: { text: "#875300", bg: "#705600" },
+            rating: 4.5,
+            rpm: 1.10
+        },
+        */
+
+export default defineType({
+    name: 'taxi',
+    title: 'Taxi',
+    type: 'document',
+    fields: [
+        {
+            name: 'name',
+            title: 'Taxi name',
+            type: 'string',
+            validation: (Rule) => Rule.required().max(20)
+        },
+        {
+            name: 'logo',
+            title: 'Logo',
+            type: 'image',
+            validation: (Rule) => Rule.required()
+        },
+        {
+            name: 'theme',
+            title: 'Color Schema',
+            type: 'string',
+            validation: (Rule) => Rule.required()
+        },
+        {
+            name: 'rating',
+            title: 'Rating',
+            type: 'number',
+            validation: (Rule) => Rule.required()
+        },
+        {
+            name: 'rpm',
+            title: 'Rate per minute',
+            type: 'number',
+            validation: (Rule) => Rule.required()
+        },
+        {
+            name: 'fee',
+            title: 'Service fee',
+            type: 'number',
+            validation: (Rule) => Rule.required()
+        },
+    ]
+})
